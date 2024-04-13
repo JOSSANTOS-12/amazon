13bb751cd294ed5618e60244956144fedca09654
JOSSANTOS-12 <agegnehuyosef1205@gmail.com>
2024-04-13 11:39:45 +0300

    I add header part

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-
+import React from "react"
+import { SlLocationPin } from "react-icons/sl";
+import './Header.css';
 const Header = () => {
     return (
         <div>
             <section>
-                <section>
-                    <div></div>
-                    <div></div>
+                <section className="upper-header">
+                    <div>
+                        <a href="#">
+                            <img src="https://m.media-amazon.com/images/I/31epF-8N9LL.png" style={{width:"80px", height:"50px"}} alt="amazon logo"/>
+                        </a>
+                    </div><span><SlLocationPin /></span>
+                    <div><p>Deliver to</p>
+                    <span>Ethiopia</span></div>
+                    <div>
+                    </div>
                     <div></div>
                 </section>
             </section>
-    </div>
+        </div>
     )
 }
 
