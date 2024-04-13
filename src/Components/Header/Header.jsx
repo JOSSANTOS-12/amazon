b27b5b710b44ad6488b4e42987965496666acc18
JOSSANTOS-12 <agegnehuyosef1205@gmail.com>
2024-04-14 00:01:51 +0300

    I add module css

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,25 +1,33 @@
 import React from "react"
 import { SlLocationPin } from "react-icons/sl";
-import './Header.css';
-const Header = () => {
-    return (
-        <div>
-            <section>
-                <section className="upper-header">
-                    <div>
-                        <a href="#">
-                            <img src="https://m.media-amazon.com/images/I/31epF-8N9LL.png" style={{width:"80px", height:"50px"}} alt="amazon logo"/>
-                        </a>
-                    </div><span><SlLocationPin /></span>
+import classes from'./header.module.css';
+const Header= () => {
+        return (
+        <>
+            <section className={classes.LHeader}>
+            <section className={classes.upper}>
+                <div>
+                <a href="/">
+                    <img
+                    src="https://m.media-amazon.com/images/I/31epF-8N9LL.png"
+                    style={{ width: "80px", height: "50px" }}
+                    alt="amazon logo"
+                    />
+                </a>
+                </div>
+                <div className={classes.location}>
+                    <span><SlLocationPin /></span>
                     <div><p>Deliver to</p>
-                    <span>Ethiopia</span></div>
-                    <div>
+                        <span>Ethiopia</span>
                     </div>
-                    <div></div>
-                </section>
+                    
+                </div>
+                <div></div>
+                <div></div>
             </section>
-        </div>
-    )
+            </section>
+        </>
+    );
 }
 
 export default Header
