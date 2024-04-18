39b950b3ac32d9d552fa9474e2b3ebe3431813a4
JOSSANTOS-12 <agegnehuyosef1205@gmail.com>
2024-04-18 21:34:15 +0300

    I add Catagory

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import Header from './Components/Header/Header'
 import Carousel from './Components/CarouselExcute/myCarousel'
-
+import CatagoryC from './Components/Catagory/CatagoryC';
 function App() {
   return (
     <>
       <Header />
-      <Carousel/>
+      <Carousel />
+      <CatagoryC />
     </>
   );
 }
--- src/Components/CarouselExcute/images/data.js
+++ src/Components/CarouselExcute/images/data.js
@@ -1,5 +1,4 @@
 import img1 from './10001.jpg';
-import img2 from "./10002.jpg";
 import img3 from "./10003.jpg";
 import img4 from "./10004.jpg";
 import img5 from "./10005.jpg";
@@ -7,4 +6,4 @@ import img6 from './10006.jpg';
 import img7 from "./10007.jpg";
 import img8 from "./10008.jpg"
 
-export const images = [img1, img2, img3, img4, img5, img6, img7, img8];
+export const images = [img1, img3, img4, img5, img6, img7, img8];
